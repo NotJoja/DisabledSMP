f2dfde39d41503fb9e2763334109b5aa62740038
NotJoja <joja.m@ilus.de>
2026-01-16 16:46:39 +0100

    Fresh Project!!!

---
+++ src/main/java/de/joja/disabledSMP/DisabledSMP.java
@@ -0,0 +1,17 @@
+package de.joja.disabledSMP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisabledSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
