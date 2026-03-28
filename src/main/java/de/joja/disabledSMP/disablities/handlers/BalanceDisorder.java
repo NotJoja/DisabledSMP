3e5208866194e6f4a60371aefb55fe85be0f91c3
NicklesTheBrainless <niconickles8@gmail.com>
2026-03-28 22:27:39 +0100

    balance disorder

--- src/main/java/de/joja/disabledSMP/disablities/handlers/BalanceDisorder.java
+++ src/main/java/de/joja/disabledSMP/disablities/handlers/BalanceDisorder.java
@@ -1,8 +1,17 @@
 package de.joja.disabledSMP.disablities.handlers;
 
+import de.joja.disabledSMP.disablities.Disability;
 import de.joja.disabledSMP.disablities.handlers.base.DisabilityHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import java.util.List;
+import java.util.UUID;
+
+import static de.joja.disabledSMP.DisabledSMP.plugin;
+import static de.joja.disabledSMP.utils.Utils.random;
 
 public class BalanceDisorder extends DisabilityHandler {
 
@@ -22,5 +31,40 @@ public class BalanceDisorder extends DisabilityHandler {
         );
 
         initIcons();
+
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+
+            for (Player player : plugin.getServer().getOnlinePlayers()) {
+
+                if (!hasDis(player, Disability.BALANCE_DISORDER))
+                    continue;
+
+                if (!player.isOnGround())
+                    continue;
+
+                Location loc = player.getLocation();
+
+                double x = loc.getX() % 1;
+                double z = loc.getZ() % 1;
+
+                boolean nearEdge = x < 0.3 || x > 0.7 || z < 0.3 || z > 0.7;
+                if (!nearEdge)
+                    continue;
+
+                if (random.nextDouble() > 0.05)
+                    continue;
+
+                Location below1 = loc.clone().add(0, -1, 0);
+                Location below2 = loc.clone().add(0, -2, 0);
+                if (below1.getBlock().isSolid() && below2.getBlock().isSolid())
+                    continue;
+
+                Vector push = new Vector(x - 0.5, 0, z - 0.5).normalize();
+                push.multiply(-0.4);
+                push.setY(0.18);
+                player.setVelocity(push);
+            }
+
+        }, 1L, 5L);
     }
 }
